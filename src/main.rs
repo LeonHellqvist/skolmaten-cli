5bf0f153d66d1f60d43454213885bbbe1e25d2be
Erik Dahlqvist <erikdahlqvist@protonmail.com>
2022-12-19 08:48:47 +0100

    Gömde undan structs i en seperat fil

--- src/main.rs
+++ src/main.rs
@@ -1,93 +1,10 @@
 use chrono::prelude::*;
 use colored::*;
 use reqwest::{Client, Error};
-use serde_derive::{Deserialize, Serialize};
-use serde_json::Value;
 use std::io::prelude::*;
 use std::{fs, env, process, io};
 
-pub type RootStation = Vec<Root2>;
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Root2 {
-    pub s: Vec<GeneratedType>,
-    pub n: String,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct GeneratedType {
-    pub i: i64,
-    pub n: String,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Root {
-    pub menu: Menu,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Menu {
-    pub is_feedback_allowed: bool,
-    pub weeks: Vec<Week>,
-    pub station: Station,
-    pub id: i64,
-    pub bulletins: Vec<Value>,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Week {
-    pub days: Vec<Day>,
-    pub week_of_year: u8,
-    pub year: i16,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Day {
-    pub reason: Option<String>,
-    pub month: u8,
-    pub day: u8,
-    pub year: i16,
-    pub meals: Option<Vec<Meal>>,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Meal {
-    pub attributes: Vec<Value>,
-    pub value: String,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Station {
-    pub url_name: String,
-    pub id: i64,
-    pub district: District,
-    pub name: String,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct District {
-    pub province: Province,
-    pub url_name: String,
-    pub id: i64,
-    pub name: String,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Province {
-    pub url_name: String,
-    pub id: i64,
-    pub name: String,
-}
+mod structs;
 
 const ID_PATH: &str = "skolmaten-cli-id.txt";
 const HELP_MESSAGE: &str = "Du kan använda funktionerna:\nsök <matsal> - söker efter en matsal\nid <matsals-id> - sätter din matsal från id\nvecka <veckonummer> - visar matsedel för specifik vecka";
@@ -145,7 +62,7 @@ async fn print_menu(week: u8) -> Result<(), Error> {
         .header("locale", "sv-SE")
         .send()
         .await?
-        .json::<Root>()
+        .json::<structs::Root>()
         .await?;
 
     const DAY_NAMES: [&str; 7] = ["Mån", "Tis", "Ons", "Tor", "Fre", "Lör", "Sön"];
@@ -205,7 +122,7 @@ async fn search(args: &Vec<String>) -> Result<(), Error> {
         .header("locale", "sv-SE")
         .send()
         .await?
-        .json::<RootStation>()
+        .json::<structs::RootStation>()
         .await?;
 
     let mut result_amount: u32 = 0;
